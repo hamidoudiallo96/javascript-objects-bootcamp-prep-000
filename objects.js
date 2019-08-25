3283be91b0a9669ea0ff79815c4e8f26d260e2b2
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-25 05:03:57 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = {
+  ledZepplin:"Stairway To Heaven"
+}
+
+var updatePlaylist = (obj,artistName,songTitle) =>{
+  return Object.assign(obj,{[artistName]:songTitle})
+}
+
+var removeFromPlaylist = (obj, artistName) =>{
+  console.log(delete obj.artistName);
+  console.log(obj)
+  return obj
+}
+
+console.log(removeFromPlaylist(playlist,ledZepplin))
