7d426389f2f6533a9218cfd1bbd05ab16fa76b87
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-25 05:06:50 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,9 +7,9 @@ var updatePlaylist = (obj,artistName,songTitle) =>{
 }
 
 var removeFromPlaylist = (obj, artistName) =>{
-  console.log(delete obj.artistName);
+  console.log(delete obj[artistName]);
   console.log(obj)
   return obj
 }
 
-console.log(removeFromPlaylist(playlist,ledZepplin))
+console.log(removeFromPlaylist(playlist,"ledZepplin"))
